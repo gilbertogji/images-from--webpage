d29164b5bc1af867daa05fa36980a559a1889acd
gilbertogji <108107239+gilbertogji@users.noreply.github.com>
2022-08-07 16:14:43 -0500

    Update and rename todas_las_imagenes3.py to todas las imagenes (codigo).py
    descriptions added and improved redaction

--- todas las imagenes (codigo).py
+++ todas las imagenes (codigo).py
@@ -4,25 +4,30 @@ import ssl
 import re
 x1x=0
 x2x=0
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+
 #obtener la pagina y parse con BeautifulSoup
-url = input('escriba la pagina: ')
+url = input('Escriba la pagina: ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 
-print('\ntodas las imagenes en la pagina:\n(se despliegan primero todas las imagenes de la pagina y posteriormente las imagenes que si nos interesan)\n(se basa en encontrar imagenes con la palabra denuncia en alguna parte de su nombre)\n')
-#print('(imagenes con attributo data-lazyload, puede haber mas imagenes, pero serian descartables')
+print('\nTodas las imagenes en la pagina:\n(se despliegan primero todas las imagenes de la pagina y posteriormente las imagenes que si nos interesan)\n(se basa en encontrar imagenes con la palabra denuncia en alguna parte de su nombre)\n')
+#las imagenes con atributos data lazyload pone los url de data-lazyload, las imagenes sin atributo lazyload pone el url del atributo src. ademas cuenta los tags imgs en total
 x=soup.find_all('img')
 for y in x:
     x1x=x1x+1
     if 'data-lazyload' in y.attrs:
-        print('url: ', y.attrs['data-lazyload'])
+        print('url(data-lazyload): ', y.attrs['data-lazyload'])
+    else:
+        if 'src' in y.attrs:
+            print('url: ', y.attrs['src'])
 
-#solo dar lo surl de las imagens que tiene denuncia en su nombre
+#solo dar la url de las imagens que tiene denuncia en su nombre
 print('\nlas imagenes que nos interesan: ')
 for y in x:
     if 'data-lazyload' in y.attrs:
